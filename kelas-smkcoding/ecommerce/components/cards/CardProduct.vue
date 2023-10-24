a030f314151719463fa12c54937c40c3e2c77923
Isjustmy <ifpersona205@gmail.com>
2023-10-25 00:10:59 +0700

    mengedit route nuxtlink

--- kelas-smkcoding/ecommerce/components/cards/CardProduct.vue
+++ kelas-smkcoding/ecommerce/components/cards/CardProduct.vue
@@ -8,23 +8,24 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="card w-120 h-auto bg-base-100 shadow-xl">
-    <figure>
-      <img
-        :src="props.product.image"
-        alt="Shoes"
-        class="w-36 h-36 object-cover"
-      />
-    </figure>
-    <div class="card-body">
-      <NuxtLink :to="`/product/${props.product.id}`">
+  <NuxtLink :to="`/product/${props.product.id}`">
+    <div class="card w-120 h-auto bg-base-100 shadow-xl">
+      <figure>
+        <img
+          :src="props.product.image"
+          alt="Shoes"
+          class="w-36 h-36 object-cover"
+        />
+      </figure>
+      <div class="card-body">
         <h1 class="card-title text-xl">{{ props.product.name }}</h1>
         <p class="text-md">{{ props.product.category }}</p>
         <p class="text-lg">{{ props.product.price }}$</p>
-      </NuxtLink>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary text-xs">Add to cart</button>
+
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary text-xs">Add to cart</button>
+        </div>
       </div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
